d7b00728279f9f4c7cbc94ed09500bbeb072082a
kanitama <jmwjmw01@gmail.com>
2016-06-14 15:51:40 +0000

    del validate isbn and change rental.all

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,6 @@ class Book < ActiveRecord::Base
     # 以下を追加
     has_one :Rental
 
-    validates :title, :author, :manufacturer, :publication_date, :isbn, :user_id, :book_code, presence: true
+    validates :title, :author, :manufacturer, :publication_date, :user_id, :book_code, presence: true
 
 end
